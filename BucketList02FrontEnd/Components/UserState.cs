91212191c974ed4fd4efecd7ed1917da56f12c89
Staf Moorkens <moorkenss0620@edu.kosh.be>
2026-03-10 13:31:33 +0100

    Fix

--- BucketList02FrontEnd/Components/UserState.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BucketList02FrontEnd.Components
-{
-    public class UserState
-    {
-        public int UserId { get; set; }
-        public string UserName { get; set; } = "";
-        public bool IsLoggedIn => UserId > 0;
-    }
-}
